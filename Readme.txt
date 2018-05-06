2cc0412db8174c216fe0f0b19654cb22aec739e2
cdbai1 <cdbai1@student.monash.edu>
2018-05-06 13:19:33 +1000

    implemented node server
    implemented node server

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+To run:
+
+Install node
+
+Navigate to directory in cmd
+
+    node server.js
+
+    localhost:8080/index.html  
+    
---
+++ server.js
@@ -0,0 +1,18 @@
+// Load HTTP module
+var http = require("http");
+var express = require('express');
+var app = express();
+
+app.use(express.static('.'));
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
