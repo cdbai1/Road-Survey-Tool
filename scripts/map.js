08689fbbbc7124368c82dfa3e44b8e37c4301d47
cdbai1 <cdbai1@student.monash.edu>
2018-05-14 00:35:33 +1000

    full functionality implemented
    massive errors in calculating road area
    needs revisiting

--- scripts/map.js
+++ scripts/map.js
@@ -1,47 +1,78 @@
+/*
+map.js
+(C) Callum Baird, 2018
+
+A script which handles the display of the embedded Google Map viewport, as well as user inputs.
+Also handles creating a Google Static Maps url to pass back to the server.
+
+Libraries used:
+Google Maps API, Google 2018 - https://cloud.google.com/maps-platform/maps/
+Google Static Maps, Google 2018 - https://developers.google.com/maps/documentation/maps-static/intro
+Node.js, Node.js Foundation 2018 - https://nodejs.org/en/
+Socket.io (via npm), Socket.io Team 2018 - https://socket.io/
+
+*/
+
+
 /*initMap()
 * initialises the google maps object
 * adds click listener to the map and handles clicks
 */
 function initMap() { // initialise the google.maps Map object 
-    var zoom = 14; // zoom level of the map    
-    var dist = 1;
-    var squarePoints = null;
+      
+    var size = 1; // size of the user selected area
+    var activeSquare = new google.maps.Polygon({}); // square currently active on map
+    var squarePoints = null; // list of points that make up the square
+    var clickPoint; // point where user clicks
+    
+    // Google Map set up
+    var zoom = 14; // initial zoom level of the map  
     var classroomLatLng = {lat: -37.911223, lng: 145.130768}; // latitude and longitude location/coordinates
     var map = new google.maps.Map(document.getElementById('map'), { // initialise a new Map object
         zoom: zoom,
         center: classroomLatLng
     });
-    var activeSquare = new google.maps.Polygon({}); // square currently active on map
+    
 
-    // event listener for user clicking a location on the map
-    // clears existing polygon
-    // generates a square on the map of size user selects 
+    // event listener for Google Map viewport
+    // handles click event
+    // generates a new square on the map of the size user selects 
     google.maps.event.addListener(map, 'click', function(event) {        
         activeSquare.setMap(null); // clear active square polygon from map
         
-        // list of points that make up the square
-        var clickPoint = event.latLng; // latlng point of user click
-        squarePoints = getSquarePath(clickPoint, dist*1000); // get the array of latlng for square corners
-        console.log(dist);
-
+        
+        clickPoint = event.latLng; // latlng point of user click
+        squarePoints = getSquarePath(clickPoint, size*1000); // get the array of latlng for square corners
         activeSquare = generatePolygon(squarePoints, map); // generate the square on the map and store object to clear later
+
+        var lngLatText = document.getElementById("outputText2");
+        lngLatText.innerHTML = "lat: " + clickPoint.lat() +"<br>lng: " + clickPoint.lng();
+
+        var sizeText = document.getElementById("outputText3");
+        sizeText.innerHTML = size + "km<sup>2</sup>";
     });
     
-    // event listener for the size selecter being changed
-    var select = document.getElementById("sizeSelect");
-    select.addEventListener("change", function(){
-        dist = select.options[select.selectedIndex].value;
-    });
 
-    // event listener for start button being clicked
-    var button = document.getElementById("startBtn");
+    // event listeners for HTML UI elements
+    // when a UI element is clicked, a message is emitted to the server
+    // data is also sent to the server
+    var socket = io.connect('localhost:8080'); // connect to the active socket
+        
+    var button = document.getElementById("startBtn"); // get button from html
     button.addEventListener("click", function(){
-        console.log("Button Clicked");
-        if(squarePoints != null){
-            generateMapURL(squarePoints);
-        }        
+        if(squarePoints != null){ // if there is an existing user selection
+            socket.emit('btnClick',{size: size, URL: generateMapURL(squarePoints)}); // emit message, with data elements size and google static maps url
+        }else{
+            alert("Please enter a selection."); // let the user know to input something first
+        }     
     });
 
+    var select = document.getElementById("sizeSelect"); // get size selector from html
+        select.value = 1; // set to 1 by default on load
+        select.addEventListener("change", function(){ // when size is changed
+            socket.emit("sizeChange", size); // emit message and size selected by user
+            size = select.options[select.selectedIndex].value; // update size var
+    });
 };
 
 
@@ -61,13 +92,9 @@ function getSquarePath(centreLngLat, metres) {
     var se = google.maps.geometry.spherical.computeOffset(ne, metres, 180); // south of ne
     var sw = google.maps.geometry.spherical.computeOffset(se, metres, 270); // west of se
 
-    console.log(nw.lat(),nw.lng());
-    console.log(ne.lat(),ne.lng());
-    console.log(se.lat(),se.lng());
-    console.log(sw.lat(),sw.lng());
-
     return [nw, ne, se, sw]; // return the path which encloses the new square
 }
+
 
 /* generatePolygon()
 * polygonPoints - path of points around the edge of the polygon
@@ -79,9 +106,9 @@ function generatePolygon(polygonPoints, map){
     var poly = new google.maps.Polygon({ // make a new square on map
           paths: polygonPoints, // using points from list argument
           strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 3,
-          fillColor: '#FF0000',
+          strokeOpacity: 0.0,
+          strokeWeight: 0,
+          fillColor: '#4d5bf9',
           fillOpacity: 0.35
         });
     poly.setMap(map); // activate on map
@@ -89,18 +116,19 @@ function generatePolygon(polygonPoints, map){
     return poly; // return the polygon object
 }
 
+
 /* generateMapURL()
 * polygonPoints - path of points around the edge of the selection
 *
 * returns the url for a Google Static Maps API image, containing a polygon with the dimensions provided
 */
 function generateMapURL(polygonPoints){
-    var URL = "https://maps.googleapis.com/maps/api/staticmap?size=500x500&style=feature:all|element:labels|visibility:off&path=weight:0|fillcolor:0x000000";
-    for (var i=0; i < polygonPoints.length; i ++){
-        URL = URL + "|" + polygonPoints[i].lat() + "," + polygonPoints[i].lng();
+    // first part of url determines colour settings and hides labels
+    var URL = "https://maps.googleapis.com/maps/api/staticmap?size=500x500&style=feature:all|element:labels|visibility:off&style=feature:all|element:geometry|color:0x000000&style=feature:road|element:geometry|visibility:simplified|color:0xffffff&path=weight:0|fillcolor:0x4d5bf9";
+    for (var i=0; i < polygonPoints.length; i ++){ // for each point in the polygon
+        URL = URL + "|" + polygonPoints[i].lat() + "," + polygonPoints[i].lng(); // append to the URL
     }    
     URL += "&key=AIzaSyA9e944TTsuLO4ffAAaBA_1KR6NCHXZKW4"; // API Key
-    console.log(URL);
     return URL;
 }
 
--- server.js
+++ server.js
@@ -1,38 +1,69 @@
-// Load HTTP module
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+/*
+server.js
+(C) Callum Baird, 2018
 
+A script which runs as the server for the Road Survey Tool
+Runs using the node platform
+Handles hosting of the HTML page as well as carrying data between the various scripts via Node.js
 
+Libraries used:
+Google Maps API, Google 2018 - https://cloud.google.com/maps-platform/maps/
+Express (via npm), Node.js Foundation 2018 - https://expressjs.com/
+Node.js, Node.js Foundation 2018 - https://nodejs.org/en/
+Socket.io (via npm), Socket.io Team 2018 - https://socket.io/
 
-app.use(express.static('.'));
+*/
 
-app.get('/', function (req, res) {
-    res.sendfile('index.html');
+var express = require('express'); // express to make an app which can see all of the files in the project
+var app = express(); // initialise app object
+var server = require('http').Server(app); // run server off of the app
+var io = require('socket.io')(server); // run socket.io off the server
+var scriptCount = 0; // keep track of how many scripts are running (socket clients)
+
+var imageURL = ""; // url for the image being shown - passed between scripts
+var size; // size selected by the user - passed between scripts
+
+/*
+* Server setup
+* Basic server that hosts my webpage as well as the associated scripts and css
+*/
+app.use(express.static('.')); // static lets the app know to use all of the files in the directory
+
+app.get('/', function (req, res) { // host index.html as the landing page
+    res.sendfile(__dirname + '/index.html');
 });
 
-//Whenever someone connects this gets executed
-io.on('connection', function(socket) {
-   console.log('A user connected');
-
-   //Whenever someone disconnects this piece of code executed
-   socket.on('disconnect', function () {
-      console.log('A user disconnected');
-   });
-   
-   socket.on('btnClick', function(){
-       console.log("they hit the button to start");
-   })
-
-    socket.on('sizeChange', function(data){
-        console.log("size got changed to:");
-        console.log(data);
-    });
-
-
-});
-
-http.listen(8080, function() {
+server.listen(8080, function() { // run the server on port 8080
    console.log('listening on localhost:8080');
 });
+
+
+/*
+* socket.io event handlers
+*
+* listen for messages broadcast by other socket connectors (the running scripts)
+* sends data to the scripts using the emit command
+*/
+io.on('connect', function(socket) { //when a script is run aka a socket connection, this will run
+    scriptCount ++; // increase the count of scripts
+    console.log(scriptCount + " script(s) running.")
+    size = 1; // set the size of the user selection to default in case they do not want to change it (no message is sent to the server)
+
+    socket.on('disconnect', function () { // if a script terminates, decrease the count
+        scriptCount --;
+        console.log(scriptCount + " script(s) running.")
+    });
+
+    socket.on('sizeChange', function(data){ // when the user changes size of their input area, this message is emitted
+        console.log("size got changed to:"); // log in console
+        console.log(data);
+        size = data; // update size variable
+    });
+
+    socket.on('btnClick',function(data){ // when the user clicks the "Find Road Area" button
+        io.emit('btnClick', {size: data.size, URL: data.URL}); // forward the data into the processImage.js script
+    });
+
+});
+
+
